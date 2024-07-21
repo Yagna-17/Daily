df4bfb5411cb3814185c2ee9e1a509d04f0662fb
Yagna-17 <yagnaprajapati17@gmail.com>
2024-07-21 18:02:08 +0530

    First Day Calculator
    Hi, It's My Day 1 Of Learing Python Language. And This Is My First Small Project.
    
    Go And Tell Me How Is That. 
    You Can Give Me Some Suaggtion Or Advice To Improve This. 
    I Will Accept Your Advice in a Good Way.
    
    So, Go And Cheak it Out.

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a = float(input("enter A :")) 
+b = float(input("enter B :")) 
+
+
+print("The Value Of", a, "+", b,"=", a+b)
+print("The Value Of", a, "-", b,"=", a-b)
+print("The Value Of", a, "*", b,"=", a*b)
+print("The Value Of", a, "/", b,"=", a/b)
